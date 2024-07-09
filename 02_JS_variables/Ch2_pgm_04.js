a31bb420d584a21177a9ad88f0f9817b7f731abd
Harini <harini@example.com>
2024-07-09 15:48:26 +0530

    update

--- 02_JS_variables/Ch2_pgm_04.js
+++ 02_JS_variables/Ch2_pgm_04.js
@@ -4,9 +4,12 @@ var score;
 score = 100;
 console.log(score);
 
-score = 150;
-console.log(score);
+var score2;
+score2 = 150;
+console.log(score2);
 
+score2 = 300;
+console.log(score2);
 
 
 /* Further Adventures
