3c638ba3b6d5904e470c9139f18d3ff81b6c53ce
Harini <harini@example.com>
2024-07-10 15:54:46 +0530

    update

--- 03_JS_Objects/Ch3_pgm_10.js
+++ 03_JS_Objects/Ch3_pgm_10.js
@@ -12,7 +12,10 @@ player1.attempted = 1;
 player1.correct = 1;
 player1.score = 50;
 
-
+console.log(player1.name);
+console.log(player1.attempted); 
+console.log(player1.correct);
+console.log(player1.score);
 
 /* Further Adventures
  *
