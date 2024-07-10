dbc7d41e0d7e63bce887b01a39f4e9869ebfba86
Harini <harini@example.com>
2024-07-10 14:34:52 +0530

    update

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -2,11 +2,37 @@
 
 var bookTitle;
 var bookAuthor;
+var book2;
 
 bookTitle = "The Hobbit";
 bookAuthor = "J. R. R. Tolkien";
+console.log(bookTitle + "by" + bookAuthor);
 
-console.log(bookTitle + " by " + bookAuthor);
+book2Title="Ignited Minds";
+book2Author="A.P.J Abdul Kalam";
+console.log(book2 +"by"+book2Author);
+
+book3Title="War and Peace";
+book3Author="Leo Tolstoy";
+console.log(book3Title + " by " + book3Author);
+
+var books=[
+      {title:"The Hobbit", author:"J.R.R Tolkien"},
+      {title:"Ignited Minds", author:"A.P.J Abdul Kalam"},
+      {title:"War and Peace", author:"Leo Tolstoy"},
+      {title:"Chitra ", author:"Rabindranath Tagore"},
+      {title:"Conquest of Self ", author:"Mahatma Gandhi"},
+      {title:"Discovery of India ", author:"Jawaharlal Nehru"},
+      {title:"Divine Life ", author:"Swami Sivananda"},
+      {title:"By God’s Decree", author:"Kapil Dev"},
+      {title:"A River Sutra ", author:"Gita Mehta"},
+      {title:"Gita Rahasya", author:"Bal Gangadhar Tilak"},
+];
+for (let i = 0; i < books.length; i++) {
+  console.log("Book " + (i + 1) + " Details:");
+  console.log("Title: " + books[i].title);
+  console.log("Author: " + books[i].author);
+}
 
 
 
