bdf3c95bb2359370f53b826f81fcbed2c9143679
Harini <harini@example.com>
2024-07-10 11:54:06 +0530

    update

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -2,10 +2,13 @@
 
 var message;
 
-message = "Hello World!";
+message = "Hello" + "World!";
 console.log(message);
 
-message = 'Congratulations! Your tweet has won a prize...';
+message = 'Congratulations!' +'Your tweet'+'has won a prize...';
+console.log(message);
+
+message = "JavaScript is fun!";
 console.log(message);
 
 
