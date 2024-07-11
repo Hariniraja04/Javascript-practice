3804df37b89fdf2d6a96a92d4636323167f3ed9e
Harini <harini@example.com>
2024-07-11 14:23:58 +0530

    update

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -13,6 +13,20 @@ player1.score = player1.score + 50;
 
 console.log(player1.name + " has scored " + player1.score);
 
+player1.score = player1.score * 1.10;
+console.log(player1.name + " has scored " + player1.score);
+
+var player2;
+
+player2 = {
+	  name: "",
+	  score: 0
+};
+player2.score = player2.score + 50;
+var totalScore = player1.score + player2.score;
+
+console.log(player1.name + " and " + player2.name + " have a total score of " + totalScore);
+
 
 
 /* Further Adventures
--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -7,6 +7,22 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+console.log("Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created: " + post.created);
+console.log("Body: " + post.body);
+
+var post2 = {
+	id: 2,
+	title: "Journey to the Center of the Earth",
+	author: "Jules",
+	created: "2023-07-11",
+	body: "Exploring the depths of our planet has never been more thrilling!"
+};
+console.log("Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created: " + post2.created);
+console.log("Body: " + post2.body);
 
 
 
