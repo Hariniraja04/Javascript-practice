94c66ce3a5488ffee132926484763becdd585897
Harini <harini@example.com>
2024-07-11 15:21:36 +0530

    update

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -14,6 +14,20 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+var movie2;
+movie2 = {
+  title: "Frenzy",
+  actor: "John Finch",
+  directors: "Alfred Hitchcok"
+};
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+
+
 
 
 /* Further Adventures
