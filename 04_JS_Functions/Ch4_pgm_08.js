4d7e85551afe5e310f9add9e467bbc6d61f10325
Harini <harini@example.com>
2024-07-12 11:33:46 +0530

    update

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -1,15 +1,25 @@
 // Displaying a menu
 
 var displayMenu;
+var displayMyMenu;
 
 displayMenu = function () {
 	console.log("Please choose an option:");
 	console.log("(1) Print log");
 	console.log("(2) Upload file");
+	console.log("(3) Download file");
+	console.log("(4) Delete file"); 
 	console.log("(9) Quit");
+};
+displayMyMenu = function () {
+	console.log("My Custom Menu:");
+	console.log("(1) View profile");
+	console.log("(2) Edit settings");
+	console.log("(3) Logout");
 };
 
 displayMenu();
+displayMyMenu();
 
 
 
