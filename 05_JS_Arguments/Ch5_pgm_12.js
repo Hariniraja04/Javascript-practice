e6f27f31066f3ca1ed43de29c43336efa7b2414a
Harini <harini@example.com>
2024-07-13 16:56:53 +0530

    update

--- 05_JS_Arguments/Ch5_pgm_12.js
+++ 05_JS_Arguments/Ch5_pgm_12.js
@@ -4,8 +4,14 @@ var player1;
 var player2;
 var showPlayerPlace;
 
-showPlayerPlace = function (playerName, playerPlace) {
-    console.log(playerName + " is in " + playerPlace);
+showPlayerPlace = function (playerName, playerPlace){
+    console.log(playerName.substring(0,1) + " is in " + playerPlace);
+};
+showPlayerPlace = function (playerName, playerPlace){
+    console.log(playerName.substring(0,2) + " is in " + playerPlace);
+};
+showPlayerPlace = function (playerName, playerPlace){
+    console.log(playerName.substring(0,3) + " is in " + playerPlace);
 };
 
 player1 = {
