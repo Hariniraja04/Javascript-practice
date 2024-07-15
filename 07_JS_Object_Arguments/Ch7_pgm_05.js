1cde4b9a87532a8b32ebb95883e2226f4ce435be
Harini <harini@example.com>
2024-07-15 14:49:29 +0530

    update

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -10,6 +10,25 @@ var showSmaller = function (num1, num2) {
 showSmaller(12, 3);
 showSmaller(-10, 3);
 
+var showLarger = function (num1, num2) {
+    var larger = Math.max(num1, num2);
+    var smaller = Math.min(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
+showLarger(12, 3); 
+showLarger(-10, 3); 
+var showSmallest = function (num1, num2, num3) {
+ var smallest = Math.min(num1, num2, num3);
+   console.log(smallest + " is the smallest");
+};
+
+showSmallest(12, 3, 7);   
+showSmallest(-10, 3, 0); 
+showSmallest(5, 7, 1);    
+
+
 
 
 /* Further Adventures
